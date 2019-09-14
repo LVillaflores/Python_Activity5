516f0489978b74f31817e8b64a29fd9f0af6b12c
LVillaflores <54790518+LVillaflores@users.noreply.github.com>
2019-09-14 11:48:48 +0800

    Add files via upload

--- Activity5/main.py
+++ Activity5/main.py
@@ -3,22 +3,32 @@ def areacalculator():
     area = 0
     pie = 3.14
     cap = _input_.capitalize()
-    if cap == "Square":
+    if cap == "1":
         side = int(input("Enter the value of side: "))
         area = area + (side ** 2)
-    elif cap == "Circle":
+    elif cap == "4":
         radius = int(input("Enter the value of radius: "))
         area = area + (2 * pie * radius)
-    elif cap == "Rectangle":
+    elif cap == "2":
         length = int(input("Enter the value of length: "))
         width = int(input("Enter the value of length: "))
         area = area + (length * width)
-    elif cap == "Triangle":
+    elif cap == "3":
         base = int(input("Enter the value of base: "))
         height = int(input("Enter the value of height: "))
         area = area +(0.5 * base * height)
+    elif cap == "5":
+        base1 = int(input("Enter the value of base 1: "))
+        base2 = int(input("Enter the value of base 2: "))
+        height = int(input("Enter the value of height: "))
+        area = area + ((base1+base2/2)*height)
     else:
         print ("Select a valid shape")
     print ("%.2f" % area)
-
+print("Menu")
+print("1 - Square")
+print("2 - Rectangle")
+print("3 - Triangle")
+print("4 - Circle")
+print("5 - Trapezoid")
 areacalculator()
